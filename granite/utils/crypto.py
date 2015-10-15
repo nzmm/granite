af13bba7fc79966d8dbae70a046c83b4b4519859
Matthew McGowan <matthew.joseph.mcgowan@gmail.com>
2015-10-16 00:16:16 +1300

    generate_secret_key now accepts path arg

--- granite/utils/crypto.py
+++ granite/utils/crypto.py
@@ -2,13 +2,15 @@ import os
 from django.utils.crypto import get_random_string
 
 
-def generate_secret_key(chars='abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'):
+def generate_secret_key(path, chars='abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'):
 
-        if not os.path.exists('.secret'):
-            os.mkdir('.secret')
+        head, tail = os.path.split(path)
+
+        if not os.path.exists(head):
+            os.mkdir(head)
 
         sk = get_random_string(50, chars)
-        # write secret to the my.secret file
-        with open('.secret/my.secret', 'w') as f:
+        # write secret to 'my.secret' file
+        with open(path, 'w') as f:
             f.write(sk)
         return sk
--- granite/utils/settings.py
+++ granite/utils/settings.py
@@ -6,7 +6,8 @@ from granite.utils.crypto import generate_secret_key
 
 def read_secret(path='.secret/my.secret'):
     if not os.path.exists(path):
-        sk = generate_secret_key()
+        sk = generate_secret_key(path)
+        print('Secret key successfully generated.')
     else:
         with open(path, 'r') as f:
             sk = f.read().strip()
