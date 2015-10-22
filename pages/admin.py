54790b43330aa3756815f4a38dce63b623ec3329
Matthew McGowan <matthew.joseph.mcgowan@gmail.com>
2015-10-22 16:57:33 +1300

    display fs_full_path

--- pages/admin.py
+++ pages/admin.py
@@ -13,7 +13,7 @@ from pages.forms import (
 
 @admin.register(Template)
 class TemplateAdmin(VersionAdmin):
-    list_display = ('handle', 'site', 'template_path')
+    list_display = ('handle', 'site', 'template_path', 'fs_full_path')
     list_filter = ('site__name',)
     readonly_fields = ('template_path',)
     form = TemplateAdminForm
