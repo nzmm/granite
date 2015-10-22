8d827c1633858dfb6db2cf2fdffe95f61bf82805
Matthew McGowan <matthew.joseph.mcgowan@gmail.com>
2015-10-22 20:36:52 +1300

    fixes

--- granite/utils/requests.py
+++ granite/utils/requests.py
@@ -6,5 +6,7 @@ from websites.models import Website
 def site_from_host(request):
     host_match = request.get_host().split('.', 1)[-1]
     try:
-        Website.objects.get(hosts__contains=host_match)
-    except Website.DoesNotExist
+        site = Website.objects.get(hosts__contains=host_match)
+    except Website.DoesNotExist:
+        return ''
+    return site
