60e7c750c5899d1ecce6c5a31a96f46052c620ac
Matthew McGowan <matthew.joseph.mcgowan@gmail.com>
2015-10-22 15:20:07 +1300

    print cwd

--- granite/wsgi.py
+++ granite/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+
+print('granite.wsgi', os.getcwd())
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "granite.settings")
 
 application = get_wsgi_application()
