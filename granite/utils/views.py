4f090d8be8846c883946f34ed7f91538ed28dd1a
Matthew McGowan <matthew.joseph.mcgowan@gmail.com>
2015-10-24 08:36:57 +1300

    print statements

--- granite/utils/views.py
+++ granite/utils/views.py
@@ -5,6 +5,7 @@ from websites.models import Website
 
 
 def std_contextual_data(request, site=None, site_handle=''):
+    print(request.get_host(), request.path)
     site = site or Website.objects.get(handle=site_handle)
     site.link_with_site_handle = get_host_matcher(request) in site.hosts
 
